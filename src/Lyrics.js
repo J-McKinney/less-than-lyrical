e96c36168df8fa8a7c3ff5ab72cad47d3543679c
Jesse McKinney <jessman51386@gmail.com>
2020-03-24 22:13:14 -0400

    yay

--- src/Lyrics.js
+++ src/Lyrics.js
@@ -1,4 +1,41 @@
 import React, { Component } from "react";
+require("dotenv").config();
+
+var CLIENTID =
+  "";
+var CLIENTSECRET =
+  "";
+var accessToken =
+  "";
+var API = "https://api.genius.com/search";
+var APISong = "https://api.genius.com/songs/";
+var songID = "";
+var maxSong = 2471960;
+var songIDArr = [];
+
+// function loopSongs() {
+//   for (var i = 0; i < 2000000000; i++) {
+//     songID += songIDArr
+//   }
+// }
+// loopSongs()
+
+var xhr = new XMLHttpRequest(); //XML HTTP Request 1,406,845,314
+xhr.onreadystatechange = function() {
+  if (xhr.readyState === 4) {
+    if (xhr.status === 200 || xhr.status === 304) {
+      // Success! Do stuff with data.
+      console.log(xhr.responseText);
+    }
+  }
+};
+xhr.open("GET", APISong + songID + accessToken, true);
+// xhr.open("GET", API+accessToken+ '&q=Kendrick%20Lamar', true);
+
+xhr.send();
+// console.log(xhr.status);
+// console.log(xhr.statusText);
+// demo = xhr.response;
 
 class Lyrics extends Component {
   render() {
